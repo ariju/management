f38194f1bbc48a336ac07b2376bcd609b09e0a72
ariju <jun.0309ari@gmail.com>
2021-03-21 17:28:34 +0900

    ログインページの作成

---
+++ pages/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Login form </v-toolbar-title>
+                <v-spacer />
+              </v-toolbar>
+              <v-card-title>
+                <h1 class="display-1">ログイン</h1>
+              </v-card-title>
+              <v-card-text>
+                <v-form class="w-full" @submit.prevent="onSubmit">
+                  <v-btn type="submit" class="info">
+                    Google でログイン
+                  </v-btn>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      const provider = new this.$firebase.auth.GoogleAuthProvider();
+      this.$fireAuth.signInWithRedirect(provider)
+    }
+  },
+};
+</script>
